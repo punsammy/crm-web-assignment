36065b70f745533f3e24c8115ee926ce343986c1
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-04-06 15:18:03 -0400

    post and params hash

--- crm.rb
+++ crm.rb
@@ -5,9 +5,9 @@ require_relative 'contact'
 require 'sinatra'
 
 # Fake data
-Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
-Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
-Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
+# Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
+# Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
+# Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
 
 get '/' do
   @crm_app_name = "Amanda's CRM"
@@ -15,9 +15,14 @@ get '/' do
 end
 
 get '/contacts' do
-  erb :contacts
+  Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
 end
 
 get '/contacts/new' do
   erb :new_contact
+end
+
+post '/contacts' do
+  Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
+  redirect to('/contacts')
 end
