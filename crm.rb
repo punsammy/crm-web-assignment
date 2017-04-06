bcf10a2dd806aaabaa15d3938b47d7a49b61b4f9
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-04-06 14:57:52 -0400

    implement view all contacts

--- crm.rb
+++ crm.rb
@@ -1,7 +1,13 @@
 # Implement the new web-based CRM here.
 # Do NOT copy the CRM class from the old crm assignment, as it won't work at all for the web-based version!
 # You'll have to implement it from scratch.
+require_relative 'contact'
 require 'sinatra'
+
+# Fake data
+Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
+Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
+Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
 
 get '/' do
   @crm_app_name = "Amanda's CRM"
